23868b06e8ae7efe77cdb353aa2b2f2ce4de026e
Manu254rce <straussmoabit@gmail.com>
2023-12-28 15:14:26 +0300

    feat: added functionality to handle package deletions

--- autoconf.py
+++ autoconf.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import subprocess
 
@@ -11,9 +10,28 @@ def install_and_update_env(package):
             print(f"An error occured while installing the package or updating the env file: {e}")
             sys.exit(1)
 
+def remove_and_update_env(paackage):
+    try:
+        subprocess.check_call(f"conda remove {package} -y", shell=True)
+        subprocess.check_call("conda env export > environment.yml", shell=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while removing the package or updating the environment file: {e}")
+        sys.exit(1)
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python autoconf.py <package>")
+    if len(sys.argv) != 3:
+        print("Usage: python autoconf.py <command> <package>")
+        print("where <command>: install, remove")
         sys.exit(1)
     else:
-        install_and_update_env(sys.argv[1])
+        command, package = sys.argv[1], sys.argv[2]
+        if command == 'install':
+            install_and_update_env(package)
+        elif command == 'remove':
+            remove_and_update_env(package)
+        else:
+            print(f"Unknown command: {command}")
+            print("Usage: python autoconf.py <command> <package>")
+            print("<command> can be 'install' or 'remove'")
+            sys.exit(1)
