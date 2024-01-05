68f37df26a348377f90723e39d55c3b6823c66d1
Manu254rce <straussmoabit@gmail.com>
2024-01-05 17:28:07 +0300

    chore:Update environment.yml and add hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Welcome to GLIESE_22")
+
+screen.setup(500,400)
+
+screen.bgcolor("lightblue")
+
+turtle.write("Welcome to GLIESE_22. Your Docker installation initialized successfully!", align="center", font=("Arial", 48, "bold"))
+turtle.done()
